9f8887cf2bd93626c6ffa60e11fa838163d1e4bc
elidallen <mitchellallen2@yahoo.com>
2023-11-07 09:56:07 -0500

    promise

--- fetchByCategory.js
+++ fetchByCategory.js
@@ -1,22 +1,26 @@
-// fetchByCategory.js
-export async function fetchJokeByCategory(category, jokeContainer) {
+export function fetchJokeByCategory(category, jokeContainer) {
+  return new Promise(async (resolve, reject) => {
     const url = `https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random?category=${category}`;
     const headers = {
       accept: "application/json",
-      "X-RapidAPI-Key": "17b8fddb2dmsh203b2ad9f88c817p14aa23jsne38b2ed7d224", 
+      "X-RapidAPI-Key": "17b8fddb2dmsh203b2ad9f88c817p14aa23jsne38b2ed7d224",
       "X-RapidAPI-Host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
     };
-  
+
     try {
       const response = await fetch(url, { method: "GET", headers });
       if (response.ok) {
         const result = await response.json();
         jokeContainer.innerHTML = `<p>${result.value}</p>`;
+        resolve(result.value);
       } else {
         jokeContainer.innerHTML = `<p>Error: Failed to fetch a Chuck Norris joke.</p>`;
+        reject("Failed to fetch a Chuck Norris joke.");
       }
     } catch (error) {
       console.error(error);
       jokeContainer.innerHTML = `<p>Error: Failed to fetch a Chuck Norris joke.</p>`;
+      reject(error);
     }
-  }
+  });
+}
